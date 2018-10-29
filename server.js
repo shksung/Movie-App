5c43810503eaee0d0b79a9bbe5c2c0e5cc3186de
Kevin Sung <s.h.kevin.sung@gmail.com>
2018-10-28 22:47:50 -0700

    search bar edit

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.get('/movie/:movieId', (req, res) => {
 })
 
 app.get('/search', (req, res) => {
-    const q = req.query.search // the part that comes after query has to equal what I put as "name" in the EJS file
-    let searchApi = "https://api.themoviedb.org/3/search/movie?api_key=9dbfde2a8b6e92417ef7a6cc5cb516fd&query=" + q
+    const search = req.query.search // the part that comes after query has to equal what I put as "name" in the EJS file
+    let searchApi = "https://api.themoviedb.org/3/search/movie?api_key=9dbfde2a8b6e92417ef7a6cc5cb516fd&query=" + search
     request(searchApi, function (err, response, body) {
         searchObject = JSON.parse(body)
         res.render('search', { movie: searchObject }) // I have to put this here or it will only render once I refresh it
